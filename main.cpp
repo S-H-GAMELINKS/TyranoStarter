6c437c02d07dade0b0ac009ccb4b7ac52e0db844
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-01-24 21:14:13 +0900

    fix main.cpp & uild tyrano.exe

--- main.cpp
+++ main.cpp
@@ -2,9 +2,11 @@
 #include <filesystem>
 #include <string>
 
-namespace sf = std::filesystem
+namespace fs = std::filesystem;
 
-void CopyTyrano(const fs::path& path, const std::string& tyrano_path, const std::string& dir){
+void CopyTyrano(const std::string& argv, const std::string& tyrano_path, const std::string& dir){
+
+    fs::path path = argv;
 
     fs::path tyrano = tyrano_path + dir;
 
@@ -17,7 +19,7 @@ int main(int argc, char* argv[]) {
 
     const std::string& tyrano_path = std::getenv("TYRANO");
 
-    CopyTyrano(argv[1], tyrano_path);
+    CopyTyrano(argv[1], tyrano_path, "\\tyrano");
 
     return 0;
 }
