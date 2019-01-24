b4754d9973c006851d7566b7a35372ddb3a6a685
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-01-24 21:09:20 +0900

    first commit

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <filesystem>
+#include <string>
+
+namespace sf = std::filesystem
+
+void CopyTyrano(const fs::path& path, const std::string& tyrano_path, const std::string& dir){
+
+    fs::path tyrano = tyrano_path + dir;
+
+    fs::copy(tyrano, path, fs::copy_options::recursive);
+
+    std::cout << "Create Tyrano Template!" << std::endl;
+}
+
+int main(int argc, char* argv[]) {
+
+    const std:string& tyrano_path = std::getenv("TYRANO");
+
+    CopyTyrano(argv[1], tyrano);
+
+    return 0;
+}
